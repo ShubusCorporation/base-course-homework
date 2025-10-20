d20fe90bcee14e1ac61695904b332ad0acbfab2a
odmitriy <shubus.corporation@gmail.com>
2025-10-20 16:48:03 +0200

    #1 Improve Election base functionality

--- Election.sol
+++ Election.sol
@@ -1,30 +1,82 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.30;
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Election {
-
-    string[] public electors;
-    //["Vasya","Taska","Basta","Papsha"]
-
-    uint256 public maxVotes;
-
+    address owner;
     uint256 public electionEndTime;
+    string[] candidates; //["Vasya","Taska","Basta","Papsha"]
+    uint256 maxVotes;
+    uint256 leader;
+    bool stopped;
 
-    mapping(address => bool) public userVotes;
+    mapping(address => bool) alreadyVoted;
+    mapping(uint256 => uint256) candidatsResult;
 
-    mapping(uint256 => uint256) public numberOfVotes;
-
-    constructor(string[] memory _electors) {
-        electors = _electors;
+    constructor(string[] memory _candidates) {
+        require(_candidates.length > 0, NoAvailableCandidates());
+        candidates = _candidates;
+        owner = msg.sender;
     }
+
+    error ElectorAlreadyVoted();
+    error CandidateNotExist(uint256 _number);
+    error OwnerCantVote();
+    error ElectionIsOver();
+    error VotingIsStopped();
+    error OnlyOwnerOperation();
+    error VotingIsNotStopped();
+    error VotingIsFailed();
+    error NoAvailableCandidates();
 
     function vote(uint256 _number) public {
-        require(userVotes[msg.sender]==false, "Your address can't vote");
-        require(_number < electors.length, "Elector does not exist");
-        userVotes[msg.sender] = true;
-        numberOfVotes[_number] += 1;
+        require(alreadyVoted[msg.sender] == false, ElectorAlreadyVoted());
+        require(_number < candidates.length, CandidateNotExist(_number));
+        require(msg.sender != owner, OwnerCantVote());
+        require(block.timestamp < electionEndTime, ElectionIsOver());
+        require(stopped == false, VotingIsStopped());
+
+        alreadyVoted[msg.sender] = true;
+        candidatsResult[_number] += 1;
+
+        if (candidatsResult[_number] > maxVotes) {
+            maxVotes = candidatsResult[_number];
+            leader = _number;
+        }
     }
 
+    function getElectionWinner() public view returns(string memory) {
+        require(stopped == true, VotingIsNotStopped());
+        require(maxVotes > 0, VotingIsFailed());
+        require(leader < candidates.length, CandidateNotExist(leader));
+
+        string memory _winner = candidates[leader];
+        return _winner;
+    }
+
+    function getCandidatesList() public view returns (string memory) {
+        require(candidates.length > 0, "No available candidates");
+        bytes memory output;
+
+        for (uint256 i = 0; i < candidates.length; i++) {
+            string memory current = string(abi.encodePacked(Strings.toString(i), " - ", candidates[i], ",  "));
+            output = abi.encodePacked(output, current);
+        }
+        return string(output);
+    }
+
+
+    function toggleVote() public {
+        require(msg.sender == owner, OnlyOwnerOperation());
+        stopped = !stopped;
+    }
+
+    function setElectionTime(uint256 _electionEndTime) public {
+        require(msg.sender == owner, OnlyOwnerOperation());
+        electionEndTime = _electionEndTime;
+    }
+
+    // Done:
     //add maxVotes functionality
     //add contract owner
     //contract owner can't vote
@@ -32,14 +84,4 @@ contract Election {
     //only owner can't stop voting
     //HARD
     //add electionTime functionality using block.timestamp
-
-
-
-
-
-
-
-
-
-
 }
